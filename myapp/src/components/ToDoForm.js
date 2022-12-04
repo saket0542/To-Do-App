c9f8c86a9c866258795490f6d90c1af02a5c2310
Saket Saraogi <saketsaraogi1@gmail.com>
2022-12-04 17:22:02 +0530

    Second Commit

---
+++ myapp/src/components/ToDoForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TodoForm = ({ handleSubmit, todo, editId, setTodo }) => {
+  return (
+    <form className="todoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input-text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit" className="ripple"> {editId ? "Edit" : "Go"}</button>
+    </form>
+  );
+};
+
+export default TodoForm;
